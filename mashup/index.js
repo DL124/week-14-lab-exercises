6e5111754d4cc674c847cfd0f0cf638e094255c4
DL124 <u2233909@live.warwick.ac.uk>
2023-02-20 11:09:45 +0000

    fixed cursed js

--- mashup/index.js
+++ mashup/index.js
@@ -1,8 +1,1 @@
-const a = 'b';
-console.log('Hello this is mashup js')
-
-if (myStatement = false)
-
-{
-  console.log('This is mashup' + 'strings'); if (true == false) { }
-}
+console.log('Hello this is mashup js');
